b9876a87e51a3b135d0583be26846aef8015a678
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-12-28 20:39:39 +0000

    add real components to routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return( 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="testing" element={<div>i'm the test page</div>} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>     
     </BrowserRouter>
   )
