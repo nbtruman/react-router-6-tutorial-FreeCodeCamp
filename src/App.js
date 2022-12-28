b8dd7fe9fa17f9656545da622df40dd425d05b68
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2022-12-28 19:52:56 +0000

    import router components and have a go

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="testing" element={<div>i'm the test page</div>} />
+
+      </Routes>     
+    </BrowserRouter>
+  )
+}
+
+
+export default App;
