b418ea0e7a09d825399ee02c662e6a1a0406753f
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2023-01-03 17:10:50 +0000

    useNavigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
-import SingleProduct from "./pages/SingleProduct"
+import SingleProduct from "./pages/SingleProduct";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
+  const [user, setUser] = useState();
   return( 
     <BrowserRouter>
       <Routes>
@@ -15,6 +19,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser}/>}/>
+          <Route path="dashboard" element={<Dashboard user={user}/>}/>
           <Route path="*" element={<Error />}/>
         </Route>
       </Routes>     
--- src/components/StyledNavBar.js
+++ src/components/StyledNavBar.js
@@ -11,7 +11,10 @@ const NavBar = () => {
             } to="/about">About</NavLink>
             <NavLink className={({isActive}) => 
                 isActive ? "link active" : "link"
-            } to="/products">Products</NavLink>          
+            } to="/products">Products</NavLink>
+            <NavLink className={({isActive}) => 
+                isActive ? "link active" : "link"
+            } to="/login">Login</NavLink>           
         </nav>
     )
 }
