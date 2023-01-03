68a33109f8b1a7caa23a810a63baf3427f1e81dd
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2023-01-03 16:44:12 +0000

    render single products

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,16 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
+
 const SingleProduct = () => {
+  const {productId} = useParams()
+  const product = products.find((product) => product.id === productId);
+  const {image, name} = product;
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <img src={image} alt="product"/>
+      <h4>{name}</h4>
+      <Link to="/products" className="btn">Back to Products</Link>
     </section>
   );
 };
