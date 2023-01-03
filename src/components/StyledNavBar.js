af0292b72878d817fad91d2bfa4f3d5fa870f948
Noah Truman <104936442+nbtruman@users.noreply.github.com>
2023-01-03 11:30:39 +0000

    NavLink

---
+++ src/components/StyledNavBar.js
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom"
+
+const NavBar = () => {
+    return(
+        <nav className="navbar">
+            <NavLink className={({isActive}) => 
+                isActive ? "link active" : "link"
+            } to="/">Home</NavLink>
+            <NavLink className={({isActive}) => 
+                isActive ? "link active" : "link"
+            } to="/about">About</NavLink>
+            <NavLink className={({isActive}) => 
+                isActive ? "link active" : "link"
+            } to="/products">Products</NavLink>          
+        </nav>
+    )
+}
+
+export default NavBar
--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import StyledNavBar from "../components/StyledNavBar";
 
 const SharedLayout = () => {
   return (
     <>
-      <NavBar />      
+      <StyledNavBar />      
       <Outlet />      
     </>
   );
